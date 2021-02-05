5c0fd7706ddf0a07b88fbc2e2fd716f1439dbddb
QinL233 <273796790@qq.com>
2021-02-05 18:02:23 +0800

    Hibernate Validator

---
+++ demo/src/main/java/com/lqz/demo/entity/Entity.java
@@ -0,0 +1,21 @@
+package com.lqz.demo.entity;
+
+/**
+ * @author LiaoQinZhou
+ * @date: 2021/2/5 18:00
+ */
+public class Entity {
+    /*
+    @Null：被注释的属性必须为null；
+    @NotNull：被注释的属性不能为null；
+    @AssertTrue：被注释的属性必须为true；
+    @AssertFalse：被注释的属性必须为false；
+    @Min：被注释的属性必须大于等于其value值；
+    @Max：被注释的属性必须小于等于其value值；
+    @Size：被注释的属性必须在其min和max值之间；
+    @Pattern：被注释的属性必须符合其regexp所定义的正则表达式；
+    @NotBlank：被注释的字符串不能为空字符串；
+    @NotEmpty：被注释的属性不能为空；
+    @Email：被注释的属性必须符合邮箱格式。
+     */
+}
