66873b984b7b3de9033fe9e08727c33606f775d8
liaoqinzhou_sz <liaoqinzhou@sz.baidu.com>
2021-07-08 11:23:05 +0800

    添加doris

--- mybatisPlusDynamic/src/test/java/com/lqz/demo/ServiceDemo.java
+++ mybatisPlusDynamic/src/test/java/com/lqz/demo/ServiceDemo.java
@@ -4,6 +4,7 @@ import com.lqz.demo.entity.Detail;
 import com.lqz.demo.entity.SysUser;
 import com.lqz.demo.service.DetailService;
 import com.lqz.demo.service.SysUserService;
+import com.lqz.demo.util.DorisStreamLoad;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,10 +19,13 @@ import java.util.List;
  */
 @SpringBootTest
 public class ServiceDemo {
+
     @Autowired
     private SysUserService userService;
     @Autowired
     private DetailService detailService;
+    @Autowired
+    private DorisStreamLoad dorisStreamLoad;
 
     @Test
     public void userTest() {
@@ -34,4 +38,18 @@ public class ServiceDemo {
         List<Detail> details = detailService.queryAll();
         System.out.println(details);
     }
+
+    @Test
+    public void put(){
+        String msg = "2021/6/1,0,万科南方,128486761,4847443537,鲲鹏-珠海红树东岸,项目词,32,3,2.36,9.38%,0.79,0,0,0,0,0\n" +
+                "2021/6/1,1,万科南方,128486761,4847443537,鲲鹏-珠海红树东岸,项目词,15,0,0,0.00%,0,0,0,0,0,0";
+        try {
+            dorisStreamLoad.sendData(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
